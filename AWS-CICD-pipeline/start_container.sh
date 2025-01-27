fcdefa46f0dba6128ca5547a11199d55b091e943
Akshay Bharadwaj <AKSHAYBHARADWAJ1998@GMAIL.COM>
2025-01-27 21:10:07 +0530

    Updated start_container.sh

--- AWS-CICD-pipeline/start_container.sh
+++ AWS-CICD-pipeline/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull akshaybharadwaj98/aws-cicd:demo-version
 
 # Run the Docker image as a container
-echo
+docker run -d -p 8000:8000 akshaybharadwaj98/aws-cicd:demo-version
