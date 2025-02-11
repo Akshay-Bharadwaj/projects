4ec36f23bfed809414d7231b51ba2d2d86b2aae0
Akshay Bharadwaj <AKSHAYBHARADWAJ1998@GMAIL.COM>
2025-02-11 19:14:18 +0530

    Update eks.tf

--- Netflix-Clone-on-EKS-Cluster/devsecops-deployment/EKS/eks.tf
+++ Netflix-Clone-on-EKS-Cluster/devsecops-deployment/EKS/eks.tf
@@ -1,3 +1,4 @@
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.1"
@@ -5,6 +6,7 @@ module "eks" {
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
 
+//add cluster addons like, coredns (serve as cluster dns and expose a static IP i.e. ClusterIP), kube-proxy (runs nodes and routes traffic in them) and cni (assign private IP addresses and create network interfaces for Pods and services in EKS cluster)
   cluster_addons = {
     coredns = {
       most_recent = true
@@ -19,30 +21,6 @@ module "eks" {
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
-  # control_plane_subnet_ids = module.vpc.intra_subnets
-
-  # # EKS Managed Node Group(s)
-  # eks_managed_node_group_defaults = {
-  #   ami_type       = "AL2_x86_64"
-  #   instance_types = ["m5.large"]
-
-  #   attach_cluster_primary_security_group = true
-  # }
-
-  eks_managed_node_groups = {
-    amc-cluster-wg = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-
-      instance_types = ["t3.large"]
-      capacity_type  = "SPOT"
-
-      tags = {
-        ExtraTag = "helloworld"
-      }
-    }
-  }
 
   tags = local.tags
 }
