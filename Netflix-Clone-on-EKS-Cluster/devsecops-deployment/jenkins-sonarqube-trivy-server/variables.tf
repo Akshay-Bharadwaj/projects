9b067bb728baf925e9dacd86f9de18c9dfa5a74a
Akshay Bharadwaj <AKSHAYBHARADWAJ1998@GMAIL.COM>
2025-02-11 19:13:49 +0530

    Update variables.tf

--- Netflix-Clone-on-EKS-Cluster/devsecops-deployment/jenkins-sonarqube-trivy-server/variables.tf
+++ Netflix-Clone-on-EKS-Cluster/devsecops-deployment/jenkins-sonarqube-trivy-server/variables.tf
@@ -1,24 +1,19 @@
 variable "vpc_id" {
-  description = "VPC ID"
-  type        = string
+  type = string
 }
 
 variable "instance_type" {
-  description = "Instance Type"
-  type        = string
+  type = string
 }
 
 variable "ami" {
-  description = "AMI ID"
-  type        = string
+  type = string
 }
 
 variable "key_pair" {
-  description = "Key Pair"
-  type        = string
+  type = string
 }
 
 variable "subnet_id" {
-  description = "Subnet ID"
-  type        = string
+  type = string
 }
