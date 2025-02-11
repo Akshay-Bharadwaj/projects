d6c21889d3cf3ad03ca9546a33e343aa52fbfe1c
Akshay Bharadwaj <AKSHAYBHARADWAJ1998@GMAIL.COM>
2025-02-11 19:15:04 +0530

    Update vpc.tf

--- Netflix-Clone-on-EKS-Cluster/devsecops-deployment/EKS/vpc.tf
+++ Netflix-Clone-on-EKS-Cluster/devsecops-deployment/EKS/vpc.tf
@@ -8,15 +8,6 @@ module "vpc" {
   azs             = local.azs
   private_subnets = local.private_subnets
   public_subnets  = local.public_subnets
-  intra_subnets   = local.intra_subnets
 
   enable_nat_gateway = true
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-  }
 }
